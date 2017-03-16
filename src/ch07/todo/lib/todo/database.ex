043b8e2dc99357e41ac46b659dafaddf914048a0
Raymond Maung <raymaung@gmail.com>
2017-03-16 13:35:52 +1100

    chapter 07 - add database workers

--- src/ch07/todo/lib/todo/database.ex
+++ src/ch07/todo/lib/todo/database.ex
@@ -10,46 +10,49 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    GenServer.cast(:database_server, {:store, key, data})
+    key
+    |> choose_worker
+    |> Todo.DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(:database_server, {:get, key})
+    key
+    |> choose_worker
+    |> Todo.DatabaseWorker.get(key)
+  end
+
+  #
+  # Choosing a worker makes a request to the :database_server process. There we
+  # keep the knowledge about our workers, and return the pid of the corresponding
+  # worker.  Once this is done, the caller process will talk to the woker directly
+  #
+  defp choose_worker(key) do
+    GenServer.call(:database_server, {:choose_worker, key})
   end
 
   def init(db_folder) do
-    File.mkdir_p(db_folder)
-    {:ok, db_folder}
+    {:ok, start_workers(db_folder)}
   end
 
-  def handle_cast({:store, key, data}, db_folder) do
-    spawn(fn ->
-      file_name(db_folder, key)
-        |> File.write!(:erlang.term_to_binary(data))
-    end)
-    {:noreply, db_folder}
+  defp start_workers(db_folder) do
+    for index <- 1..3, into: HashDict.new do
+      {:ok, pid} = Todo.DatabaseWorker.start(db_folder)
+      {index - 1, pid}
+    end
   end
 
-  def handle_call({:get, key}, caller, db_folder) do
-    #
-    # Spawn a new reader
-    #
-    spawn(fn ->
-      data = case File.read(file_name(db_folder, key)) do
-        {:ok, contents} -> :erlang.binary_to_term(contents)
-        _ -> nil
-      end
-
-      #
-      # Responds from the spawned process
-      GenServer.reply(caller, data)
-    end)
-
-    #
-    # No reply from the database
-    #
-    {:noreply, db_folder}
+  def handle_call({:choose_worker, key}, _, workers) do
+    worker_key = :erlang.phash2(key, 3)
+    {:reply, HashDict.get(workers, worker_key), workers}
   end
 
-  defp file_name(db_folder, key), do: "#{db_folder}/#{key}"
+  # Needed for testing
+  def handle_info(:stop, workers) do
+    workers
+    |> HashDict.values
+    |> Enum.each(&send(&1, :stop))
+
+    {:stop, :normal, HashDict.new}
+  end
+  def handle_info(_, state), do: {:noreply, state}
 end
